38dd3b8e83592ac3ef4c1898a1ae1aba1c156fe3
Frederic P <fredericparrel@gmail.com>
2016-03-08 23:17:47 +0100

    LAMP test added

--- toprod.py
+++ toprod.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 
 def in_ignore(root,file):
     # root
@@ -44,33 +45,61 @@ def in_ignore(root,file):
         return True
     return False
 
+def open_mkdir(fname,row):
+    path = os.path.dirname(fname)
+    print 'path='+path
+    if not os.path.exists(path):
+        print 'mkdir -p '+path
+        os.makedirs(path)
+    return open(fname,row)
+
+instantiate_new = False
+test_sys = False
+
+if len(sys.argv)>1 and sys.argv[1] in ('-h','--help'):
+    print 'Usage: python toprod.py [--new]'
+    exit(0)
+elif len(sys.argv)>1 and sys.argv[1] in ('-n','--new'):
+    instantiate_new = True
+if len(sys.argv)>2 and sys.argv[2] in ('-t','--test'):
+    test_sys = True
+
 print 'Run translate.py and minify.sh first'
-for root, dirs, files in os.walk("."):
+if test_sys:
+    dest_dir = 'lamp-test/'
+else:
+    dest_dir = 'lamp-prod/'
+for root, dirs, files in os.walk("wamp"):
     for file in files:
         if in_ignore(root,file):
             continue
+        is_new = False
         if file.endswith('.php') or file.endswith('.txt') or file.endswith('.apk') or file.endswith('.html') or file.endswith('.css') or file.endswith('.js') or file.endswith('.png') or file.endswith('.gif') or file.endswith('.jpg') or file.endswith('.svg') or file.endswith('.htc') or file.endswith('.py') or file=='.htaccess':
             f_from = open('%s/%s'%(root,file),'r')
             try:
-                f_to = open('../lamp/%s/%s'%(root,file),'r')
+                f_to = open('%s/%s'%(root.replace('wamp/',dest_dir),file),'r')
             except IOError:
                 print '+  %s/%s'%(root,file)
-                f_from.close()
-                continue
+                if instantiate_new:
+                    f_to = open_mkdir('%s/%s'%(root.replace('wamp/',dest_dir),file),'w')
+                    is_new = True
+                else:
+                    f_from.close()
+                    continue
             c_from = f_from.read()
             modified=False
             if file.endswith('.php'):
-                if c_from.find('http://localhost')>-1:
+                if c_from.find('http://localhost')>-1 and not test_sys:
                     modified=True
                     c_from = c_from.replace('http://localhost','http://regepe.com')
-                if c_from.find('.js"')>-1:
+                if c_from.find('.js"')>-1 and not test_sys:
                     modified=True
                     c_from = c_from.replace('.js"','.min.js"')
             elif file.endswith('.js'):
-                if c_from.find('i30ryoc55sqaj25dkn2ek85y')>-1:
+                if c_from.find('${GeoPortalApiKey}')>-1:
                     modified=True
                     c_from = c_from.replace('${GeoPortalApiKey}','${GeoPortalApiKeyProd}')
-            elif file=='config.py':
+            elif file=='config.py' and not test_sys:
                 if c_from.find("domain = 'localhost'")>-1:
                     modified=True
                     c_from = c_from.replace("domain = 'localhost'","domain = 'regepe.com'")
@@ -79,9 +108,12 @@ for root, dirs, files in os.walk("."):
                     modified=True
                     c_from = c_from.replace('#!c:/Python27/python.exe','#!/usr/bin/python')
             f_from.close()
-            c_to = f_to.read()
+            if instantiate_new and is_new:
+                f_to.write(c_from)
+            else:
+                c_to = f_to.read()
             f_to.close()
-            if c_from!=c_to:
+            if not is_new and c_from!=c_to:
                 if modified:
                     print 'D! %s/%s'%(root,file)
                     #print len(c_from),len(c_to)
