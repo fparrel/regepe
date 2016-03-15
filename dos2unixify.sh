b5be5b2ee5db060bf13cf6c858cee782f857cb1b
fparrel <fredericparrel@gmail.com>
2016-03-15 17:47:24 +0100

    CRLF + realign.py now handling wamp-src and wamp-test + src realigned with prod

---
+++ dos2unixify.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+	exit -1
+elif [[ $1 == --help ]]; then
+	echo "Usage: $0 directory"
+else
+	for i in `find $1/ -name "*.py" -o -name "*.php" -o -name "*.js" -o -name "*.css" -o -name "*.html"`; do dos2unix $i; done
+fi
