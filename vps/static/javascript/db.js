fa4ef1f61376e2120b84506e664fd0eee0e89dce
fparrel <fredericparrel@gmail.com>
2016-12-23 21:39:11 +0100

    Minor fixes + indentation on .js files + unittests + automatic directory creation

--- vps/static/javascript/db.js
+++ vps/static/javascript/db.js
@@ -38,9 +38,9 @@ function putToDbNoAck(mapid,pwd,ele,val) {
 }
 
 function getFromDb(mapid,ele) {
-  document.getElementById(ele).innerHTML = "Retrieving...";
+	document.getElementById(ele).innerHTML = "Retrieving...";
 	var url = "/dbget/" + encodeURIComponent(mapid) + "/" + encodeURIComponent(ele);
-  var req = new XMLHttpRequest();
+	var req = new XMLHttpRequest();
 	req.open("GET", url, true);
 	req.onreadystatechange = onDbAnswer;
 	req.send(null);
@@ -48,19 +48,19 @@ function getFromDb(mapid,ele) {
 
 function getFromDbWithCallback(mapid,ele,callback) {
 	var url = "/dbget/" + encodeURIComponent(mapid) + "/" + encodeURIComponent(ele);
-  var req = new XMLHttpRequest();
+	var req = new XMLHttpRequest();
 	req.open("GET", url, true);
-  req.callback = callback;
+	req.callback = callback;
 	req.onreadystatechange = onDbAnswerWithCallback;
 	req.send(null);
 }
 
 function getFromDbWithCallbackError(mapid,ele,callback,errorcallback) {
 	var url = "/dbget/" + encodeURIComponent(mapid) + "/" + encodeURIComponent(ele);
-  var req = new XMLHttpRequest();
+	var req = new XMLHttpRequest();
 	req.open("GET", url, true);
-  req.callback = callback;
-  req.errorcallback = errorcallback;
+	req.callback = callback;
+	req.errorcallback = errorcallback;
 	req.onreadystatechange = onDbAnswerWithCallback;
 	req.send(null);
 }
@@ -70,21 +70,21 @@ function onDbAnswer() {
 	if (this.readyState == 4) {
 		if (this.status == 200) {
 			var message = this.responseXML.getElementsByTagName("message")[0].childNodes[0].nodeValue;
-      var pageelementid = this.responseXML.getElementsByTagName("pageelementid")[0].childNodes[0].nodeValue;
-      var value;
-      if (this.responseXML.getElementsByTagName("value")[0].childNodes.length<1) {
-        value = '';
-      }
-      else {
-        value = this.responseXML.getElementsByTagName("value")[0].childNodes[0].nodeValue;
-      }
-      if (message=="OK") {
+			var pageelementid = this.responseXML.getElementsByTagName("pageelementid")[0].childNodes[0].nodeValue;
+			var value;
+			if (this.responseXML.getElementsByTagName("value")[0].childNodes.length<1) {
+				value = '';
+			}
+			else {
+				value = this.responseXML.getElementsByTagName("value")[0].childNodes[0].nodeValue;
+			}
+			if (message=="OK") {
 				//stop progress bar
-        document.getElementById(pageelementid).innerHTML = value;
-        if(pageelementid=='trackuser') {
-          document.getElementById("trackuserlink").href = '/showuser.php?user='+value;
-        }
-      }
+				document.getElementById(pageelementid).innerHTML = value;
+				if(pageelementid=='trackuser') {
+					document.getElementById("trackuserlink").href = '/showuser/'+value;
+				}
+			}
 			else {
 				//display error icon
 				document.getElementById(pageelementid).innerHTML = message;
@@ -97,24 +97,24 @@ function onDbAnswerWithCallback() {
 	if (this.readyState == 4) {
 		if (this.status == 200) {
 			var message = this.responseXML.getElementsByTagName("message")[0].childNodes[0].nodeValue;
-      var pageelementid = this.responseXML.getElementsByTagName("pageelementid")[0].childNodes[0].nodeValue;
-      var value;
-      if (this.responseXML.getElementsByTagName("value")[0].childNodes.length<1) {
-        value = '';
-      }
-      else {
-        value = this.responseXML.getElementsByTagName("value")[0].childNodes[0].nodeValue;
-      }
-      if (message=="OK") {
-        this.callback(value);
+			var pageelementid = this.responseXML.getElementsByTagName("pageelementid")[0].childNodes[0].nodeValue;
+			var value;
+			if (this.responseXML.getElementsByTagName("value")[0].childNodes.length<1) {
+				value = '';
 			}
 			else {
-        if (typeof this.errorcallback!="undefined") {
-          this.errorcallback(message);
-        }
-        else {
-          /* error ignored */
-        }
+				value = this.responseXML.getElementsByTagName("value")[0].childNodes[0].nodeValue;
+			}
+			if (message=="OK") {
+				this.callback(value);
+			}
+			else {
+				if (typeof this.errorcallback!="undefined") {
+					this.errorcallback(message);
+				}
+				else {
+					/* error ignored */
+				}
 			}
 		}
 	}
@@ -134,7 +134,7 @@ function hideInput(ele) {
 
 function onOkClick(ele,mapid) {
 	var newvalue = document.getElementById(ele+'_inputtxtbox').value;
-  hideInput(ele);
+	hideInput(ele);
 	putToDb(mapid,getPwdFromCookie(mapid),ele,newvalue);
 }
 
@@ -151,7 +151,7 @@ function onInputKeyPress(ev,ele) {
 		//ev = event;
 		characterCode = ev.keyCode //character code is contained in IE's keyCode property
 	}
-  if (characterCode == 27) {
-    hideInput(ele);
-  }
+	if (characterCode == 27) {
+		hideInput(ele);
+	}
 }
