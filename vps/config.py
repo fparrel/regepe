dff4bc11a7a64eb75b0986e630a624ddb81beb9e
fparrel <fredericparrel@gmail.com>
2016-12-25 21:23:41 +0100

    config moved to other file, server entry point file renamed

--- vps/config.py
+++ vps/config.py
@@ -1,2 +1,12 @@
+import json
+
+def readKeysAndPasswords(filename):
+    f=open(filename,'r')
+    k = json.load(f)
+    f.close()
+    return k
+
+# Load keys and password
+keysnpwds=readKeysAndPasswords('config/keysnpwds-test.json')
 
 domain = 'localhost'
