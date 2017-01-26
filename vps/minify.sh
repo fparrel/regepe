ece16b28a896632075e498b64f986211531fcbb0
Frederic P <fredericparrel@gmail.com>
2017-01-27 00:56:03 +0100

    clever minifiy

--- vps/minify.sh
+++ vps/minify.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 for i in `ls static/javascript/*.js | grep -v "\.min\.js"`; do
-	echo "Processing $i"
-	java -jar ../external_tools/yuicompressor-2.4.8.jar $i -o `echo $i | sed 's/\.js/\.min\.js/'`
+	MINIFIED=`echo $i | sed 's/\.js/\.min\.js/'`
+	if [ ! -f $MINIFIED ] || [ `stat -c %Y $i` -gt `stat -c %Y $MINIFIED` ]; then
+		echo "Processing $i"
+		java -jar ../external_tools/yuicompressor-2.4.8.jar $i -o $MINIFIED
+	fi
 done
 
