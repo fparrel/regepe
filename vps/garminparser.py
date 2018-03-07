ce1c0ef287ec31371b68fb06a1e4325edad201c2
fparrel <info@regepe.com>
2018-03-07 17:44:40 +0100

    Avoid mixed http inside https + garmin course support added + allow rebuild tool to handle traces from url

--- vps/garminparser.py
+++ vps/garminparser.py
@@ -8,10 +8,22 @@ import datetime
 
 import json
 
+def ParseJsonGarmingFileCourse(inputfile,trk_id,trk_seg_id):
+    ptlist = []
+    data = inputfile.read()
+    js = json.loads(data)
+    return map(lambda pt:Point(pt['latitude'],pt['longitude'],pt['elevation'],None,None,datetime.datetime.fromtimestamp(pt['timestamp']/1000)),js['geoPoints'])
+
+
 def ParseJsonGarminFile(inputfile,trk_id,trk_seg_id):
     ptlist = []
     data = inputfile.read()
     js = json.loads(data)['com.garmin.activity.details.json.ActivityDetails']
+    latidx = -1
+    lonidx = -1
+    eleidx = -1
+    spdidx = -1
+    timeidx = -1
     for measurement in js['measurements']:
         if measurement['key']=='directLatitude':
             latidx = measurement['metricsIndex']
@@ -24,6 +36,12 @@ def ParseJsonGarminFile(inputfile,trk_id,trk_seg_id):
             spdunit = measurement['unit']
         elif measurement['key']=='directTimestamp':
             timeidx = measurement['metricsIndex']
+    if latidx == -1 or lonidx == -1:
+        raise Exception('lat/lon not found in activity json')
+    if eleidx == -1:
+        raise Exception('elevation not found in activity json')
+    if spdidx == -1 or timeidx == -1:
+        raise Exception('speed or time not found in activity json')
     return map(lambda metric: Point(float(metric['metrics'][latidx]),float(metric['metrics'][lonidx]),float(metric['metrics'][eleidx]),float(metric['metrics'][spdidx]),None,datetime.datetime.fromtimestamp(int(metric['metrics'][timeidx]/1000)),spdunit),js['metrics'])
 
 
